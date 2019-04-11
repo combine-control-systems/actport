4ffffdda5782b48af999014294ce3f8cb7b43c1b
Stefan Larsson <stefan.larsson@combine.se>
2019-04-11 12:14:28 +0200

    Added Matrix Concatenation.

---
+++ src/main/scala/actport/generators/MatrixConcatenation.scala
@@ -0,0 +1,28 @@
+package actport.generators
+
+import actport.ActivateBlock
+import actport.simulink._
+
+import scala.collection.JavaConverters._
+
+object MatrixConcatenation extends Generator[ActivateBlock] {
+  override def apply(path: SimulinkPath)(implicit block: ActivateBlock): Seq[Expression] = {
+    val blockPath = path / block.name
+
+    val p = block.parameters.asScala
+
+    Seq(
+      AddBlock(Simulink.MathOperations.MatrixConcatenate, blockPath),
+      SetParam(blockPath, SimulinkParameterName("NumInputs"), p.get("nin") match {
+        case Some(inputCount: String) => inputCount
+        case _ => "2"
+      }),
+      SetParam(blockPath, SimulinkParameterName("Mode"), "Multidimensional array"),
+      SetParam(blockPath, SimulinkParameterName("ConcatenateDimension"), p.get("method") match {
+        case Some("'Horizontal'") => "2"
+        case Some("'Vertical'") => "1"
+        case _ => "1"
+      })
+    ) ++ commonProperties(path)
+  }
+}
--- src/main/scala/actport/generators/package.scala
+++ src/main/scala/actport/generators/package.scala
@@ -17,6 +17,7 @@ package object generators {
       case "system/Dynamical/Derivative" => generators.Derivative(path)
       case "system/Dynamical/Integral" => generators.Integral(path)
       case "system/MathOperations/Gain" => generators.Gain(path)
+      case "system/MatrixOperations/MatrixConcatenation" => generators.MatrixConcatenation(path)
       case "system/MathOperations/MathFunc" => generators.MathFunc(path)
       case "system/MathOperations/Sum" => generators.Sum(path)
       case "system/MatrixOperations/MatrixGain" => generators.MatrixGain(path)
