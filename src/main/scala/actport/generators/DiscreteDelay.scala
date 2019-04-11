bff21769d330209aed25e949f7103bdd4a92356a
Stefan Larsson <stefan.larsson@combine.se>
2019-04-11 12:56:35 +0200

    Added Discrete Delay.

---
+++ src/main/scala/actport/generators/DiscreteDelay.scala
@@ -0,0 +1,23 @@
+package actport.generators
+
+import actport.ActivateBlock
+import actport.simulink._
+
+import scala.collection.JavaConverters._
+
+object DiscreteDelay extends Generator[ActivateBlock] {
+  override def apply(path: SimulinkPath)(implicit block: ActivateBlock): Seq[Expression] = {
+    val blockPath = path / block.name
+
+    val p = block.parameters.asScala
+
+    // TODO: Handle datatype.
+    Seq(
+      AddBlock(Simulink.Discrete.UnitDelay, blockPath),
+      SetParam(blockPath, SimulinkParameterName("InitialCondition"), p.get("init_cond") match {
+        case Some(initialCondition: String) => initialCondition
+        case _ => "0"
+      })
+    ) ++ commonProperties(path)
+  }
+}
