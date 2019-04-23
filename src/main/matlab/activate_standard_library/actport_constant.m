824bf9a758fb365be8902d916b1f936f35c93380
Pablo Correa Gómez <ablocorrea@hotmail.com>
2019-04-23 11:34:07 +0200

    Fixed name for actport_constant

--- src/main/matlab/activate_standard_library/actport_constant.m
+++ src/main/matlab/activate_standard_library/actport_constant.m
@@ -3,7 +3,7 @@ function out = actport_constant(diagram, block)
     import actport.GeneratorApi.*
 
     block = addBlockExpr(block, 'simulink/Sources/Constant');
-    block = setParamExpr(block, 'Value', getParameter(block, 'Value', '1'));
+    block = setParamExpr(block, 'Value', getParameter(block, 'C', '1'));
 
     block = addCommonProperties(block);
     out = updateDiagram(diagram, block);
