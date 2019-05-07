3c52dfbbb55dcdd8c1aa5d6458016edbd71357b0
Pablo Correa Gómez <pablo.correa.gomez@combine.se>
2019-05-07 12:00:32 +0200

    Fixed spelling breaking some generators

--- src/main/matlab/activate_standard_library/actport_annotation.m
+++ src/main/matlab/activate_standard_library/actport_annotation.m
@@ -6,5 +6,5 @@ function model = actport_annotation(model, block_id, model_path)
     text = get_parameter(model, block_id, 'txt', 'No text...');
     block_path = sprintf('%s/%s', model_path, text);
 
-    add_block('built-in/Area', block_path, 'Position', get_position(model, block_id));
+    add_block('built-in/Area', block_path, 'Position', str2num(get_position(model, block_id)));
 end
