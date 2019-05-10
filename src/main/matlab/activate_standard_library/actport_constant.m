443c047b1dc933420cc598e4775ae57eee2abf69
Stefan Larsson <stefan.larsson@combine.se>
2019-05-10 09:56:26 +0200

    Do not interpret vector parameters as 1-D in constant block.

--- src/main/matlab/activate_standard_library/actport_constant.m
+++ src/main/matlab/activate_standard_library/actport_constant.m
@@ -9,6 +9,8 @@ function model = actport_constant(model, block_id, model_path)
 
     value = get_parameter(model, block_id, 'C', '1');
     set_param(block_path, 'Value', value);
+    % Ensure that signals keep their dimensions.
+    set_param(block_path, 'VectorParams1D', 'off');
 
     set_common_parameters(model, block_id, model_path);
 end
