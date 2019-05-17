816ca3bbe5d2ea437403a999250d101fa31cce7a
Stefan Larsson <stefan.larsson@combine.se>
2019-05-17 14:26:13 +0200

    Added 3D lookup table. Fixes #49.

--- src/main/matlab/activate_standard_library/actport_lookup_table_3D.m
+++ src/main/matlab/activate_standard_library/actport_lookup_table_3D.m
@@ -1,37 +1,41 @@
-% activate = 'system/LookupTables/LookupTable2D'
-function model = actport_lookup_table_2D(model, block_id, model_path)
+% activate = 'system/LookupTables/LookupTable3D'
+function model = actport_lookup_table_3D(model, block_id, model_path)
     import actport.model.Matlab.*
 
     name = get_name(model, block_id);
     block_path = sprintf('%s/%s', model_path, name);
 
-    add_block('simulink/Lookup Tables/2-D Lookup Table', block_path);
+    add_block('simulink/Lookup Tables/n-D Lookup Table', block_path);
 
-    row_indices = get_parameter(model, block_id, 'xx', '[-1, 1]');
-    column_indices = get_parameter(model, block_id, 'yy', '[-1, 1]');
-    table = get_parameter(model, block_id, 'zz', '[-1, -1; 1, 1]');
+    x = get_parameter(model, block_id, 'xx', '[-1, 1]');
+    y = get_parameter(model, block_id, 'yy', '[-1, 1]');
+    z = get_parameter(model, block_id, 'zz', '[-1, 1]');
+    f = get_parameter(model, block_id, 'ff', '[-1, 1, -1, 1, -1, 1, -1, 1]');
 
-    set_param(block_path, 'BreakpointsForDimension1', row_indices);
-    set_param(block_path, 'BreakpointsForDimension2', column_indices);
-    set_param(block_path, 'Table', table);
+    xs = length(eval(x));
+    ys = length(eval(y));
+    zs = length(eval(z));
+    f = sprintf('reshape(%s, %d, %d, %d)', f, xs, ys, zs);
 
-    interpolation = strrep(get_parameter(model, block_id, 'Method', 'Bilinear interpolation'), '''', '');
+    set_param(block_path, 'NumberOfTableDimensions', '3');
+    set_param(block_path, 'BreakpointsForDimension1', x);
+    set_param(block_path, 'BreakpointsForDimension2', y);
+    set_param(block_path, 'BreakpointsForDimension3', z);
+    set_param(block_path, 'Table', f);
+
+    interpolation = strrep(get_parameter(model, block_id, 'Method', ''), '''', '');
     switch interpolation
-        case 'Bilinear interpolation'
-            simulink_interpolation = 'Linear point-slope';
+        case 'Trilinear interpolation'
+            simulink_interpolation = 'Linear'
         case 'Nearest_point'
-            simulink_interpolation = 'Nearest';
+            simulink_interpolation = 'Nearest'
         case 'Point_just_below'
-            simulink_interpolation = 'Nearest';
+            simulink_interpolation = 'Nearest'
             warning('Simulink does not support Point just below interpolation - falling back to Nearest.');
         case 'Point_just_above'
-            simulink_interpolation = 'Nearest';
+            simulink_interpolation = 'Nearest'
             warning('Simulink does not support Point just above interpolation - falling back to Nearest.');
-        otherwise
-            simulink_interpolation = 'Linear point-slope';
-            warning(sprintf('Unknown interpolation method %s - falling back to Bilinear Interpolation', method));
     end
-    set_param(block_path, 'InterpMethod', simulink_interpolation);
 
     extrapolation = strrep(get_parameter(model, block_id, 'DoOutside', 'Hold'), '''', '');
     switch extrapolation
