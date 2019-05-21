88d4a1f59d3aaefc2e9f14fb2a2f223b5ca7c731
Pablo Correa Gómez <pablo.correa.gomez@combine.se>
2019-05-21 12:17:16 +0200

    Step generator arranged to work with booleans

--- src/main/matlab/activate_standard_library/actport_step_generator.m
+++ src/main/matlab/activate_standard_library/actport_step_generator.m
@@ -7,7 +7,7 @@ function model = actport_step_generator(model, block_id, model_path)
 
     add_block('simulink/Sources/Step', block_path);
     set_param(block_path, 'Time', get_parameter(model, block_id, 'steptime', '1'));
-    set_param(block_path, 'Before', get_parameter(model, block_id, 'iniv', '0'));
-    set_param(block_path, 'After', get_parameter(model, block_id, 'finv', '1'));
+    set_param(block_path, 'Before', bool_translation(get_parameter(model, block_id, 'iniv', '0')));
+    set_param(block_path, 'After', bool_translation(get_parameter(model, block_id, 'finv', '1')));
     set_common_parameters(model, block_id, model_path);
 end
