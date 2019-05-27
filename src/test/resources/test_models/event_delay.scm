5f567dc89e5e64d512433046029efe4c245b982b
Stefan Larsson <stefan.larsson@combine.se>
2019-05-27 16:21:37 +0200

    Added event delay. Fixes #37.

---
+++ src/test/resources/test_models/event_delay.scm
@@ -0,0 +1,323 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<model name="event_delay">
+  <header formatversion="2" hwxversion="2019.2" script="oml"/>
+  <edition>
+    <properties>
+      <property name="creator" value="Stefan Larsson"/>
+      <property name="creationdate" value="05/27/19 14:03:58"/>
+      <property name="revision" value="0"/>
+      <property name="lastmodifieddate" value="05/27/19 14:16:13"/>
+      <property name="comments" value=""/>
+    </properties>
+  </edition>
+  <initialization/>
+  <finalization/>
+  <diagram>
+    <block name="EventDelay" type="block">
+      <template name="system/ActivationOperations/EventDelay"/>
+      <properties>
+        <property name="inlinable" value="0"/>
+        <property name="masked" value="1"/>
+        <property name="atomic" value="0"/>
+      </properties>
+      <graphics>
+        <label position="bottom" visible="0" font="Arial,11" color="255,255,255,255"/>
+        <frame shape="rectangle" visible="1" color="0,0,0,255" thickness="1.5"/>
+        <background color="255,255,255,255"/>
+        <position x="380.6792907714844" y="158.81846618652344"/>
+        <size width="80" height="40"/>
+        <rotate angle="0"/>
+        <texts>
+          <text name="text1" color="0,0,0,255" clip="1" x="0.5" y="0.5" rotate="1" visible="1" font="Arial,12" align="center">Event Delay&#xA;1</text>
+        </texts>
+        <images>
+          <image name="image1" file="" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+        </images>
+        <flip value="0">
+          <images>
+            <image name="image2" file="" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+          </images>
+        </flip>
+      </graphics>
+      <ports>
+        <port name="" type="fixedport">
+          <properties>
+            <property name="iotype" value="out"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="1"/>
+          </properties>
+          <graphics>
+            <location position="bottom"/>
+            <label visible="1" font="Arial,10" color="255,255,255,255"/>
+            <frame shape="triangle" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+        <port name="" type="fixedport">
+          <properties>
+            <property name="iotype" value="in"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="1"/>
+          </properties>
+          <graphics>
+            <location position="top"/>
+            <label visible="1" font="Arial,10" color="255,255,255,255"/>
+            <frame shape="triangle" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+      </ports>
+      <parameters>
+        <section name="Parameters">
+          <parameter name="delay" value="1"/>
+        </section>
+      </parameters>
+    </block>
+    <block name="EventClock" type="block">
+      <template name="system/ActivationOperations/EventClock"/>
+      <properties>
+        <property name="masked" value="1"/>
+        <property name="inlinable" value="1"/>
+        <property name="atomic" value="0"/>
+        <property name="status" value="1"/>
+      </properties>
+      <graphics>
+        <label position="top" visible="0" font="Arial,11" color="255,255,255,255"/>
+        <frame shape="rectangle" visible="1" color="0,0,0,0" thickness="1.5"/>
+        <background color="255,255,255,0"/>
+        <position x="395.6792907714844" y="58.81846618652344"/>
+        <size width="50" height="40"/>
+        <rotate angle="0"/>
+        <texts>
+          <text name="text1" color="0,0,0,255" clip="1" x="0.5" y="0.5" rotate="1" visible="1" font="Arial,12" align="center"/>
+        </texts>
+        <images>
+          <image name="image1" file="ActivationOperations/time.svg" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+        </images>
+        <flip value="0">
+          <images>
+            <image name="image2" file="ActivationOperations/time.svg" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+          </images>
+        </flip>
+      </graphics>
+      <ports>
+        <port name="" type="fixedport">
+          <properties>
+            <property name="iotype" value="out"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="1"/>
+          </properties>
+          <graphics>
+            <location position="bottom"/>
+            <label visible="1" font="Arial,10" color="255,255,255,255"/>
+            <frame shape="triangle" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+      </ports>
+      <parameters>
+        <section name="Parameters">
+          <parameter name="period" value="5"/>
+          <parameter name="init" value="0"/>
+        </section>
+      </parameters>
+    </block>
+    <block name="EventScope" type="block">
+      <template name="system/SignalViewers/EventScope"/>
+      <properties>
+        <property name="inlinable" value="0"/>
+        <property name="masked" value="1"/>
+        <property name="atomic" value="0"/>
+      </properties>
+      <graphics>
+        <label position="bottom" visible="0" font="Arial,11" color="255,255,255,255"/>
+        <frame shape="rectangle" visible="1" color="0,0,0,255" thickness="1.5"/>
+        <background color="255,255,255,255"/>
+        <position x="395.6792907714843" y="292.8184814453125"/>
+        <size width="40" height="40"/>
+        <rotate angle="0"/>
+        <texts>
+          <text name="text1" color="0,0,0,255" clip="1" x="0.5" y="0.5" rotate="1" visible="1" font="Arial,12" align="center"/>
+        </texts>
+        <images>
+          <image name="image1" file="SignalViewers/scope.svg" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+        </images>
+        <flip value="0">
+          <images>
+            <image name="image2" file="SignalViewers/scope.svg" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+          </images>
+        </flip>
+      </graphics>
+      <ports>
+        <port name="&quot;1&quot; &quot;2&quot;" type="variableport">
+          <properties>
+            <property name="iotype" value="in"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="nin"/>
+          </properties>
+          <graphics>
+            <location position="top"/>
+            <label visible="1" font="Arial,11" color="255,255,255,255"/>
+            <frame shape="triangle" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+      </ports>
+      <parameters>
+        <section name="Main">
+          <parameter name="nin" value="2"/>
+          <parameter name="winSize" value="[-1,-1]"/>
+          <parameter name="winPos" value="[-1,-1]"/>
+          <parameter name="winNam" value="&apos;&apos;"/>
+          <parameter name="winHidden" value="1"/>
+          <parameter name="showAtEnd" value="0"/>
+          <parameter name="option" value="&apos;Reuse&apos;"/>
+        </section>
+      </parameters>
+    </block>
+    <block name="ActivationSplit" type="activation_split">
+      <template name="system/Links/ActivationSplit"/>
+      <properties>
+        <property name="inlinable" value="0"/>
+        <property name="masked" value="1"/>
+        <property name="atomic" value="0"/>
+      </properties>
+      <graphics>
+        <label position="bottom" visible="0" font="Arial,11" color="255,255,255,255"/>
+        <frame shape="ellipse" visible="1" color="183,15,19,255" thickness="1.5"/>
+        <background color="183,15,19,255"/>
+        <position x="420.6792907714844" y="129.81846618652344"/>
+        <size width="0" height="0"/>
+        <rotate angle="0"/>
+        <texts>
+          <text name="text1" color="0,0,0,255" clip="1" x="0.5" y="0.5" rotate="1" visible="1" font="Arial,12" align="center"/>
+        </texts>
+        <images>
+          <image name="image1" file="" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+        </images>
+        <flip value="0">
+          <images>
+            <image name="image2" file="" visible="1" alpha="255" aspectratio="1" x="0" y="0" width="1" height="1" clip="1" rotate="1"/>
+          </images>
+        </flip>
+      </graphics>
+      <ports>
+        <port name="" type="fixedport">
+          <properties>
+            <property name="iotype" value="in"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="1"/>
+          </properties>
+          <graphics>
+            <location position="left"/>
+            <label visible="1" font="Arial,10" color="255,255,255,255"/>
+            <frame shape="point" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+        <port name="" type="fixedport">
+          <properties>
+            <property name="iotype" value="out"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="1"/>
+          </properties>
+          <graphics>
+            <location position="right"/>
+            <label visible="1" font="Arial,10" color="255,255,255,255"/>
+            <frame shape="point" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+        <port name="" type="fixedport">
+          <properties>
+            <property name="iotype" value="out"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="2"/>
+          </properties>
+          <graphics>
+            <location position="right"/>
+            <label visible="1" font="Arial,10" color="255,255,255,255"/>
+            <frame shape="point" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+        <port name="" type="fixedport">
+          <properties>
+            <property name="iotype" value="out"/>
+            <property name="datatype" value="Activation"/>
+            <property name="portnumber" value="3"/>
+          </properties>
+          <graphics>
+            <location position="right"/>
+            <label visible="1" font="Arial,10" color="255,255,255,255"/>
+            <frame shape="point" visible="1" color="183,15,19,255" thickness="1.5"/>
+            <background color="183,15,19,255"/>
+          </graphics>
+        </port>
+      </ports>
+      <parameters>
+        <section name="Parameters">
+        </section>
+      </parameters>
+    </block>
+    <link name="" type="Activation">
+      <label position="bottom" visible="0" font="Arial,11" color="255,255,255,255"/>
+      <from block="EventClock" port="1" iotype="out"/>
+      <to block="ActivationSplit" port="1" iotype="in"/>
+      <pen shape="line" color="183,15,19,255" thickness="1.5"/>
+      <points/>
+    </link>
+    <link name="" type="Activation">
+      <label position="bottom" visible="0" font="Arial,11" color="255,255,255,255"/>
+      <from block="EventDelay" port="1" iotype="out"/>
+      <to block="EventScope" port="2" iotype="in"/>
+      <pen shape="line" color="183,15,19,255" thickness="1.5"/>
+      <points>
+        <point x="420.6792907714843" y="282.8184814453187"/>
+      </points>
+    </link>
+    <link name="" type="Activation">
+      <label position="bottom" visible="0" font="Arial,11" color="255,255,255,255"/>
+      <from block="ActivationSplit" port="1" iotype="out"/>
+      <to block="EventDelay" port="1" iotype="in"/>
+      <pen shape="line" color="183,15,19,255" thickness="1.5"/>
+      <points/>
+    </link>
+    <link name="" type="Activation">
+      <label position="bottom" visible="0" font="Arial,11" color="255,255,255,255"/>
+      <from block="ActivationSplit" port="2" iotype="out"/>
+      <to block="EventScope" port="1" iotype="in"/>
+      <pen shape="line" color="183,15,19,255" thickness="1.5"/>
+      <points>
+        <point x="365.6792907714843" y="129.81846618652318"/>
+        <point x="365.6792907714844" y="228.81846618652344"/>
+        <point x="405.6792907714842" y="228.8184661865232"/>
+      </points>
+    </link>
+    <context/>
+    <graphics>
+      <viewport width="800" height="800" topleftx="0" toplefty="0"/>
+      <window width="800" height="800" topleftx="0" toplefty="0"/>
+    </graphics>
+  </diagram>
+  <simulation>
+    <properties>
+      <property name="InitialTime" value="0"/>
+      <property name="FinalTime" value="30"/>
+      <property name="RealTimeScale" value="0"/>
+      <property name="AbsoluteErrorTolerance" value="0.000001"/>
+      <property name="RelativeErrorTolerance" value="0.000001"/>
+      <property name="MaxTimeInterval" value="-1"/>
+      <property name="TimeTolerance" value="-1"/>
+      <property name="Solver" value="lsodar"/>
+      <property name="MaxStepSize" value="-1"/>
+      <property name="InitialStepSize" value="-1"/>
+      <property name="MinimalStepSize" value="-1"/>
+      <property name="ZeroCrossingThreshold" value="-1"/>
+      <property name="NumberOfConsecutiveZeroCrossing" value="-1"/>
+      <property name="TimeToleranceZeroCrossing" value="-1"/>
+      <property name="AlgebraicSolver" value="idacalc"/>
+      <property name="JacobianMethod" value="1"/>
+    </properties>
+  </simulation>
+</model>
